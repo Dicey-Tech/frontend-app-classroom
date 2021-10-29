785de1eecbc7b128425d770b6a6d77fc10745993
Salik Rafiq <chameeyass@hotmail.com>
2021-10-29 11:55:16 +0100

    Add in new es5 compatiable query string component.
    Modify LMS API to use this.

--- src/app/services/LmsApiService.js
+++ src/app/services/LmsApiService.js
@@ -1,46 +1,7 @@
 /* eslint-disable eqeqeq */
 import { getAuthenticatedHttpClient } from '@edx/frontend-platform/auth';
+import queryString from 'query-string-for-all';
 import configuration from '../../config';
-
-const courseList = [
-  {
-    courseId: 1,
-    title: 'This is a handy course 1 title',
-    description: 'This is a sample description',
-    imageURL: '/image1.jpg',
-  },
-  {
-    courseId: 2,
-    title: 'Another handy course 2 title',
-    description: 'This is a sample description',
-    imageURL: '/image2.jpg',
-  },
-  {
-    courseId: 3,
-    title: 'Another handy course 3 title',
-    description: 'This is a sample description',
-    imageURL: '/image3.jpg',
-  },
-  {
-    courseId: 4,
-    title: 'Another handy course 4 title',
-    description: 'This is a sample description',
-    imageURL: '/image3.jpg',
-  },
-  {
-    courseId: 5,
-    title: 'Another handy course 5 title',
-    description: 'This is a sample description',
-    imageURL: '/image3.jpg',
-  },
-  {
-    courseId: 6,
-    title: 'Another handy course 6 title',
-    description: 'This is a sample description',
-    imageURL: '/image3.jpg',
-  },
-
-];
 
 class LmsApiService {
   static apiClient = getAuthenticatedHttpClient;
@@ -57,43 +18,25 @@ class LmsApiService {
     return LmsApiService.apiClient().get(requestUrl);
   }
 
-  /* says "all" but there could be thousands of courses so I force a page */
-  /* eslint-disable */
-  static async fetchAllCourses(page, pageSize) {
-    const queryParams = {
-      page,
-      page_size: pageSize,
-    };
-    // const requestUrl = `${LmsApiService.baseUrl}/api/courses/v1/courses/?${qs.stringify(queryParams)}`;
-
-    return {
-      data: {
-        page: 1,
-        pageCount: 1,
-        pageSize: 20,
-        courseCount: courseList.length,
-        courses: courseList,
-      },
-    };
-  }
-  /* eslint-enable */
-
   static async fetchEnterpriseQuery(queryParams) {
-    const requestUrl = `${LmsApiService.baseUrl}/enterprise/api/v1/enterprise-customer/?page=${queryParams.page}&uuid=${queryParams.uuid}`;
+    const requestUrl = `${LmsApiService.baseUrl}/enterprise/api/v1/enterprise-customer/?${queryString.stringify(queryParams)}`;
     return LmsApiService.apiClient().get(requestUrl);
   }
 
   static async fetchEnterpriseByUuid(uuid) {
     const queryParams = {
-      page: 1,
       uuid,
     };
     return this.fetchEnterpriseQuery(queryParams);
   }
 
   static async fetchEnterpriseBySlug(slug) {
-    const requestUrl = `${LmsApiService.baseUrl}/enterprise/api/v1/enterprise-customer/?page=1&slug=${slug}`;
-    return LmsApiService.apiClient().get(requestUrl);
+    const queryParams = {
+      slug,
+    };
+    return this.fetchEnterpriseQuery(queryParams);
+    // const requestUrl = `${LmsApiService.baseUrl}/enterprise/api/v1/enterprise-customer/?page=1&slug=${slug}`;
+    // return LmsApiService.apiClient().get(requestUrl);
   }
 }
 
